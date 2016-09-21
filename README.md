f6324dce5dc2703fd2653e5312e5d1e28206e98b
Rebecca <rlmuraya@gmail.com>
2016-09-20 22:24:42 -0400

    Initial commit - i3, terminal, vim, git

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles configuration
+
+To set up:
+
+    git clone https://github.com/rlmuraya/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,20 @@
+# i3 configuration.
+mkdir -p ~/.i3
+ln -sf $PWD/configs/i3 ~/.i3/config
+
+# Terminal configuration.
+mkdir -p ~/.config/xfce4/terminal
+ln -sf $PWD/config/terminal ~/.config/xfce4/terminal/terminalrc
+
+# Vim configuration.
+mkdir -p ~/.vim/bundle/
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
+    git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+fi
+ln -sf $PWD/configs/vim ~/.vimrc
+
+# Git configuration.
+ln -sf $PWD/configs/git ~/.gitconfig
